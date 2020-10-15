18135c6f8456be965a1961294bcd9eb69cd75d88
Shane Slone <slone.shane@gmail.com>
2020-10-15 18:29:20 -0400

    added several new test

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -19,9 +19,61 @@ describe('User Form', () => {
         submitBtn().should('exist')
     })
 
-    describe('Filling out the form', () => {
+    describe('Filling out the form works', () => {
         it('submit button starts disabled', () => {
             submitBtn().should('be.disabled')
         })
+
+        it('can fill out form inputs', () => {
+            submitBtn().should('be.disabled')
+            nameInput()
+                .should('have.value', '')
+                .type('Shane Slone')
+                .should('have.value', 'Shane Slone')
+            emailInput()
+                .should('have.value', '')
+                .type('slone.shane@gmail.com')
+                .should('have.value', 'slone.shane@gmail.com')
+            passwordInput()
+                .should('have.value', '')
+                .type('testpassword')
+                .should('have.value', 'testpassword')
+            roleSelect()
+                .should('have.value', '')
+                .select('student')
+                .should('have.value', 'student')
+            tosInput()
+                .should('not.be.checked')
+                .check()
+                .should('be.checked')
+            submitBtn().should('not.be.disabled')
+        })
     })
+
+    describe('adding a new user', () => {
+        it('submitting the form', () => {
+            cy.contains(/Shane Slone/).should('not.exist')
+            nameInput().type('Shane Slone')
+            emailInput().type('slone.shane@gmail.com')
+            passwordInput().type('testpassword')
+            roleSelect().select('student')
+            tosInput().check()
+            submitBtn().click()
+            cy.contains(/Shane Slone/).should('exist')
+        })
+    })
+    
+    describe('error messages display', () => {
+        it('displays an error if password is not 6 chars long', () => {
+            cy.contains('Password must me 6 characters long.').should('not.exist')
+            passwordInput().type('123')
+            cy.contains('Password must me 6 characters long.').should('exist')
+        })
+        it('displays an error if email is not valid', () => {
+            cy.contains('Please enter a valid email.').should('not.exist')
+            emailInput().type('my email')
+            cy.contains('Please enter a valid email.').should('exist')
+        })
+    })
+    
 })
